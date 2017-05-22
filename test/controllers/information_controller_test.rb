115ef127ca8a5d3c6eabc14e2ce0fa86534d840f
Sergio Ocon <sergio.ocon@redhat.com>
2017-05-22 15:36:08 +0200

    ssl

---
+++ test/controllers/information_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class InformationControllerTest < ActionDispatch::IntegrationTest
+  test "should get Info" do
+    get information_info_url
+    assert_response :success
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+end
