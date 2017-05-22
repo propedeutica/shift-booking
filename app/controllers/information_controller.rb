d62b7d43fa32ebcb55bc32a707da8e05e221c599
Sergio Ocon <sergio.ocon@redhat.com>
2017-05-22 14:27:32 +0200

    minimal config

---
+++ app/controllers/information_controller.rb
@@ -0,0 +1,7 @@
+class InformationController < ApplicationController
+  def info
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'information/info'
+  get 'information/about'
+  root 'information#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
