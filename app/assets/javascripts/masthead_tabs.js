05b018a8760b3e71b1a17738cb1ce391d7b4b194
Sergio Ocon <sergio.ocon@redhat.com>
2017-05-24 10:25:16 +0200

    deleting one reload

--- app/assets/javascripts/masthead_tabs.js
+++ app/assets/javascripts/masthead_tabs.js
@@ -1,12 +1,13 @@
-$(function() {
-  // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    // save the latest tab; use cookies if you like 'em better:
-    localStorage.setItem('lastTab', $(this).attr('href'));
-  });
-  // go to the latest tab, if it exists:
-  var lastTab = localStorage.getItem('lastTab');
-  if (lastTab) {
-    $('[href="' + lastTab + '"]').tab('show');
-  }
+$(document).ready(function() {
+// matchHeight the contents of each .card-pf and then the .card-pf itself
+  $(".row-cards-pf > [class*='col'] > .card-pf .card-pf-title").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-body").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-footer").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf").matchHeight();
+
+// initialize tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+// Initialize the vertical navigation
+  $().setupVerticalNavigation(true);
 });
