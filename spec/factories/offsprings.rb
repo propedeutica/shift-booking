e594d3848cf7b23ebb18b8060b915f1af2e9c105
Sergio Ocon <sergio.ocon@redhat.com>
2017-05-25 18:42:42 +0200

    factory and translation

--- spec/factories/offsprings.rb
+++ spec/factories/offsprings.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :offspring do
     first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    grade { Offspring.grades.keys.sample }
+    last_name  { Faker::Name.last_name }
+    grade      { Offspring.grades.keys.sample }
+    age        { Faker::Number.between(8, 10) }
     user
   end
 end
